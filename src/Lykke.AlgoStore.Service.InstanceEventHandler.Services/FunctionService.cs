352660fd9e4ec68b17d2a54660b1b0104a7b0dd7
Miladin Joksic <miladin.joksic@primeholding.com>
2018-07-23 10:59:37 +0200

    Added validation in functions service

--- src/Lykke.AlgoStore.Service.InstanceEventHandler.Services/FunctionService.cs
+++ src/Lykke.AlgoStore.Service.InstanceEventHandler.Services/FunctionService.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Lykke.AlgoStore.Algo.Charting;
 using Lykke.AlgoStore.Service.InstanceEventHandler.Core.Repositories;
 using Lykke.AlgoStore.Service.InstanceEventHandler.Core.Services;
+using Lykke.AlgoStore.Service.InstanceEventHandler.Services.Utils;
 
 namespace Lykke.AlgoStore.Service.InstanceEventHandler.Services
 {
@@ -24,6 +27,8 @@ namespace Lykke.AlgoStore.Service.InstanceEventHandler.Services
         {
             var functionChartingUpdates = functions.ToList();
 
+            ValidateFunctionChartingUpdateData(functionChartingUpdates);
+
             //Store function values
             await _functionChartingUpdateRepository.WriteAsync(functionChartingUpdates);
 
@@ -32,6 +37,29 @@ namespace Lykke.AlgoStore.Service.InstanceEventHandler.Services
             {
                 await _functionHandler.Handle(Mapper.Map<FunctionChartingUpdate>(function));
             }
+        }
+
+        private static void ValidateFunctionChartingUpdateData(List<FunctionChartingUpdate> functionChartingUpdateData)
+        {
+            if(functionChartingUpdateData == null)
+                throw new ArgumentNullException(nameof(functionChartingUpdateData));
+
+            if(!functionChartingUpdateData.Any())
+                throw new ValidationException("Function values cannot be empty");
+
+            if (functionChartingUpdateData.Count > 100)
+                throw new ValidationException("Cannot save more then 100 function chart update records per batch");
+
+            var flattenedData = functionChartingUpdateData.Flatten(x => x.InnerFunctions).ToList();
+
+            if(flattenedData.Select(x => x.InstanceId).Distinct().Count() > 1)
+                throw new ValidationException("InstanceId must be the same for all function values");
+
+            if(flattenedData.Any(x => string.IsNullOrEmpty(x.InstanceId)))
+                throw new ValidationException("All function values must have InstanceId provided");
+
+            if(flattenedData.Any(x => string.IsNullOrEmpty(x.FunctionName)))
+                throw new ValidationException("All function names must be provided");
         }
     }
 }
