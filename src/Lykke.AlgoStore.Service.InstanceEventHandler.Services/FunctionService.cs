4b5897bb47c384e5bf260145294bcb8cc7e8eb50
Miladin Joksic <miladin.joksic@primeholding.com>
2018-07-23 11:06:49 +0200

    moved string error messages into resource file

--- src/Lykke.AlgoStore.Service.InstanceEventHandler.Services/FunctionService.cs
+++ src/Lykke.AlgoStore.Service.InstanceEventHandler.Services/FunctionService.cs
@@ -7,6 +7,7 @@ using AutoMapper;
 using Lykke.AlgoStore.Algo.Charting;
 using Lykke.AlgoStore.Service.InstanceEventHandler.Core.Repositories;
 using Lykke.AlgoStore.Service.InstanceEventHandler.Core.Services;
+using Lykke.AlgoStore.Service.InstanceEventHandler.Services.Strings;
 using Lykke.AlgoStore.Service.InstanceEventHandler.Services.Utils;
 
 namespace Lykke.AlgoStore.Service.InstanceEventHandler.Services
@@ -45,21 +46,21 @@ namespace Lykke.AlgoStore.Service.InstanceEventHandler.Services
                 throw new ArgumentNullException(nameof(functionChartingUpdateData));
 
             if(!functionChartingUpdateData.Any())
-                throw new ValidationException("Function values cannot be empty");
+                throw new ValidationException(Phrases.FunctionValuesCannotBeEmpty);
 
             if (functionChartingUpdateData.Count > 100)
-                throw new ValidationException("Cannot save more then 100 function chart update records per batch");
+                throw new ValidationException(Phrases.MaxRecordsPerBatchReached);
 
             var flattenedData = functionChartingUpdateData.Flatten(x => x.InnerFunctions).ToList();
 
             if(flattenedData.Select(x => x.InstanceId).Distinct().Count() > 1)
-                throw new ValidationException("InstanceId must be the same for all function values");
+                throw new ValidationException(Phrases.SameInstanceIdForAllFunctionValues);
 
             if(flattenedData.Any(x => string.IsNullOrEmpty(x.InstanceId)))
-                throw new ValidationException("All function values must have InstanceId provided");
+                throw new ValidationException(Phrases.InstanceIdForAllFunctionValues);
 
             if(flattenedData.Any(x => string.IsNullOrEmpty(x.FunctionName)))
-                throw new ValidationException("All function names must be provided");
+                throw new ValidationException(Phrases.AllFunctionNamesMustBeProvided);
         }
     }
 }
