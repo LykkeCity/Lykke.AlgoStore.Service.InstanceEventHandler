8bb5344778994f65d1e5684fcc1e9100c432cd52
Miladin Joksic <miladin.joksic@primeholding.com>
2018-08-08 11:27:57 +0200

    Fixed wrong status code returned for validation errors

---
+++ src/Lykke.AlgoStore.Service.InstanceEventHandler/Infrastructure/GlobalErrorHandlerMiddleware.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Common.Log;
+using Lykke.Common.Log;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Lykke.AlgoStore.Service.InstanceEventHandler.Infrastructure
+{
+    public class GlobalErrorHandlerMiddleware
+    {
+        private readonly ILog _log;
+        private readonly RequestDelegate _next;
+
+        public GlobalErrorHandlerMiddleware(RequestDelegate next, ILogFactory logFactory)
+        {
+            if (logFactory == null)
+            {
+                throw new ArgumentNullException(nameof(logFactory));
+            }
+
+            _log = logFactory.CreateLog(this);
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                await LogError(context, ex);
+                await CreateErrorResponse(context, ex);
+            }
+        }
+
+        private async Task LogError(HttpContext context, Exception ex)
+        {
+            var url = context.Request?.GetUri()?.AbsoluteUri;
+            var urlWithoutQuery = RequestUtils.GetUrlWithoutQuery(url) ?? "?";
+            var body = await RequestUtils.GetRequestPartialBodyAsync(context);
+
+            _log.Error(urlWithoutQuery, ex, null, new { url, body });
+        }
+
+        private async Task CreateErrorResponse(HttpContext ctx, Exception ex)
+        {
+            ctx.Response.ContentType = "application/json";
+
+            string errorMessage;
+
+            switch (ex)
+            {
+                case ArgumentNullException ane:
+                    errorMessage = $"Invalid argument: {ane.Message}";
+                    ctx.Response.StatusCode = 400;
+                    break;
+
+                case InvalidOperationException ioe:
+                    errorMessage = $"Invalid operation: {ioe.Message}";
+                    ctx.Response.StatusCode = 400;
+                    break;
+
+                case ValidationException ve:
+                    errorMessage = $"Validation error: {ve.Message}";
+                    ctx.Response.StatusCode = 400;
+                    break;
+
+                default:
+                    errorMessage = "Technical problem";
+                    ctx.Response.StatusCode = 500;
+                    break;
+            }
+
+            var responseJson = JsonConvert.SerializeObject(errorMessage);
+
+            await ctx.Response.WriteAsync(responseJson);
+        }
+    }
+}
---
+++ src/Lykke.AlgoStore.Service.InstanceEventHandler/Infrastructure/RequestUtils.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Http;
+
+namespace Lykke.AlgoStore.Service.InstanceEventHandler.Infrastructure
+{
+    //REMARK: This is same class as in Lykke.Common.ApiLibrary
+    //Lykke plans to update their common api library and make this public
+    //and also, to allow easy override of global error handler middleware
+    public static class RequestUtils
+    {
+        public static async Task<string> GetRequestPartialBodyAsync(HttpContext context)
+        {
+            if (context?.Request?.Body == null)
+            {
+                return null;
+            }
+
+            // request body might be already read at the moment 
+            if (context.Request.Body.CanSeek)
+            {
+                context.Request.Body.Seek(0, SeekOrigin.Begin);
+            }
+
+            // 64 Kb - is max size for the azure queue message
+            const int maxBytesToRead = 1024 * 64;
+            var bodyBytes = new byte[maxBytesToRead];
+            var bodyBytesCount = await context.Request.Body.ReadAsync(bodyBytes, 0, maxBytesToRead);
+
+            return Encoding.UTF8.GetString(bodyBytes, 0, bodyBytesCount);
+        }
+
+        [CanBeNull]
+        public static string GetUrlWithoutQuery([CanBeNull] string url)
+        {
+            if (url == null)
+            {
+                return null;
+            }
+
+            var index = url.IndexOf('?');
+            var urlWithoutQuery = index == -1 ? url : url.Substring(0, index);
+
+            return urlWithoutQuery;
+        }
+    }
+}
